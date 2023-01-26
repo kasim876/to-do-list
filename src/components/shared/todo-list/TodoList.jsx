e873461799d9af4ef8c8a69c69d9a7057c632fc2
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-26 21:36:08 +0200

    edit todo-list component

--- src/components/shared/todo-list/TodoList.jsx
+++ src/components/shared/todo-list/TodoList.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
-import {TransitionGroup, CSSTransition} from 'react-transition-group';
 
 import TodoItem from '../todo-item/TodoItem';
 
 import styles from './TodoList.module.scss';
 
 const TodoList = ({todos, title, complete, remove}) => {
+  const completedTodos = todos.filter(t => t.isCompleted === true);
+  const unCompletedTodos = todos.filter(t => t.isCompleted === false);
 
   return (
     <div>
       <h1 align="center" className={styles.title}>{title}</h1>
       <div className={styles.root}>
-        {todos.length === 0
-          ?
-          <h2 className={styles.placeholder}>Нет задач</h2>
-          :
-          <TransitionGroup>
-            {
-              todos.map(todo => (
-                <CSSTransition
-                  key={todo.id}
-                  timeout={500}
-                  classNames="todo"
-                >
-                  <TodoItem todo={todo} complete={complete} remove={remove}/>
-                </CSSTransition>
-              ))
-            }
-          </TransitionGroup>
+        {
+          unCompletedTodos.length === 0
+            ?
+            <h2 className={styles.placeholder}>Нет задач</h2>
+            :
+            unCompletedTodos.map(todo => (
+              <TodoItem todo={todo} complete={complete} remove={remove} key={todo.id}/>
+            ))
+        }
+        {
+          completedTodos.length !== 0
+            ?
+            <>
+              <h2 className={styles.subtitle}>Завершенные</h2>
+              {
+                completedTodos.map(todo => (
+                  <TodoItem todo={todo} complete={complete} remove={remove} key={todo.id}/>
+                ))
+              }
+            </>
+            :
+            ''
         }
       </div>
     </div>
