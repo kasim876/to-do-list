cac531f5dcab78690cd978ecb41bf0d30695e64e
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-27 22:07:56 +0200

    edit todo-list component

--- src/components/shared/todo-list/TodoList.jsx
+++ src/components/shared/todo-list/TodoList.jsx
@@ -4,37 +4,36 @@ import TodoItem from '../todo-item/TodoItem';
 
 import styles from './TodoList.module.scss';
 
-const TodoList = ({todos, complete, remove}) => {
-  const completedTodos = todos.filter(t => t.isCompleted === true);
-  const unCompletedTodos = todos.filter(t => t.isCompleted === false);
-
+const TodoList = ({completedTodos, inCompletedTodos, complete, remove}) => {
   return (
-    <div>
-      <div className={styles.root}>
-        {
-          unCompletedTodos.length === 0
-            ?
-            <h2 className={styles.placeholder}>Нет задач</h2>
-            :
-            unCompletedTodos.map(todo => (
-              <TodoItem todo={todo} complete={complete} remove={remove} key={todo.id}/>
-            ))
-        }
-        {
-          completedTodos.length !== 0
-            ?
-            <>
-              <h2 className={styles.subtitle}>Завершенные</h2>
-              {
-                completedTodos.map(todo => (
-                  <TodoItem todo={todo} complete={complete} remove={remove} key={todo.id}/>
-                ))
-              }
-            </>
-            :
-            ''
-        }
-      </div>
+    <div className={styles.root}>
+      {
+        inCompletedTodos.length === 0
+        &&
+        <h2 className={styles.placeholder}>Нет задач</h2>
+      }
+
+      {
+        inCompletedTodos.length !== 0
+        &&
+        inCompletedTodos.map(todo => (
+          <TodoItem todo={todo} complete={complete} remove={remove} key={todo.id}/>
+        ))
+      }
+
+      {
+        completedTodos.length !== 0
+        &&
+        <h2 className={styles.subtitle}>Завершенные</h2>
+      }
+
+      {
+        completedTodos.length !== 0
+        &&
+        completedTodos.map(todo => (
+          <TodoItem todo={todo} complete={complete} remove={remove} key={todo.id}/>
+        ))
+      }
     </div>
   );
 };
