112bb745348ca26725d717b81d7b84def1c1dd5a
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-01-29 09:33:11 +0200

    edit calendar store

--- src/store/CalendarStore.js
+++ src/store/CalendarStore.js
@@ -4,6 +4,7 @@ class CalendarStore {
   constructor() {
     this._visible = false;
     this._value = null;
+    this._coords = {x: 0, y: 0};
     makeAutoObservable(this);
   }
 
@@ -15,12 +16,21 @@ class CalendarStore {
     this._value = value;
   }
 
+  setCoords(x, y) {
+    this._coords.x = x;
+    this._coords.y = y;
+  }
+
   get visible() {
     return this._visible;
   }
 
   get value() {
     return this._value;
+  }
+
+  get coords() {
+    return this._coords;
   }
 }
 
